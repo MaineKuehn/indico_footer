f288bd1a02bf226b19ca6562b5d8fd4b85f62550
Max Fischer <maxfischer2781@gmail.com>
2018-05-29 18:40:54 +0200

    added disclaimer to all code files

--- indico_footer/utility.py
+++ indico_footer/utility.py
@@ -1,3 +1,20 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the Indico Footer Customisation Plugin.
+# Copyright (C) 2018 Eileen Kuehn, Max Fischer
+#
+# This is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Indico Footer Customisation Plugin;if not, see <http://www.gnu.org/licenses/>.
 from indico.util.i18n import make_bound_gettext
 
 gettext = make_bound_gettext('custom_footer')
